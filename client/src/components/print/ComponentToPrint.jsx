e96cb9fc25a2508d541ee567ba45a57cfe80fce3
kyawphyothu <kyawphyothukpt256@gmail.com>
2023-11-15 22:11:48 +0630

    test voucher6

--- client/src/components/print/ComponentToPrint.jsx
+++ client/src/components/print/ComponentToPrint.jsx
@@ -1,5 +1,5 @@
 import React, { forwardRef } from 'react'
-import "./style.css"
+// import "./style.css"
 import { Chip, Divider, Grid, Stack, Typography } from '@mui/material';
 import FormatCode from '../../helper/FormetCode';
 import CalculateWeight from '../../helper/CalculateWeight';
@@ -11,16 +11,136 @@ import { red } from '@mui/material/colors';
 const ComponentToPrint = forwardRef((props, ref) => {
 	const { order, histories } = props;
 
-	const divs = [];
-
-	// Using a loop to create 20 div elements
-	for (let i = 1; i <= 100; i++) {
-		divs.push(<div key={i} style={{ color: "red" }}>Div {i}</div>);
-	}
-
 	return (
-		<div ref={ref}>
-			{divs}
+		<div ref={ref} style={styles.main}>
+			<div style={{ textAlign: "center" }}>
+				<Typography variant='subtitle1' sx={{ fontWeight: "600" }}>သျှီ အပေါင်ဆိုင်</Typography>
+			</div>
+
+			<Stack>
+				<Typography variant='subtitle2'>{order.name}</Typography>
+				<Typography variant='body2'>{FormatCode(order.code)}</Typography>
+				<Typography variant='body2'>{order.village}</Typography>
+				<Typography variant='body2'>{order.phone}</Typography>
+				<Typography variant='body2'>{order.gold}</Typography>
+				<Typography variant='body2'>{CalculateWeight(order.weight)}</Typography>
+				<Typography variant='body2'>{GetMMDate(new Date(order.date))}</Typography>
+				<span>
+					<CustomBadge>{order.acceptor === 'အိမ်' ? 'h' :
+						order.acceptor === 'အေးအေးခိုင်' ? 'kk' :
+						order.acceptor === 'စန်စန်းထွေး' ? 'ss' :
+						order.acceptor === 'ဥမ္မာဝင်း' ? 'ww' :
+						order.acceptor}
+					</CustomBadge>
+					{
+						Boolean(order.redeem) && (
+							<CustomBadge color='error'>ရွေးပြီး</CustomBadge>
+						)
+					}
+				</span>
+			</Stack>
+
+
+			<Grid container spacing={3} mb={3}>
+				<Grid item xs={12}>
+					<Divider sx={{ color: "red" }}>
+						<Chip color='primary' label="မှတ်တမ်းများ" />
+					</Divider>
+				</Grid>
+				{histories.length &&
+					histories.map((history) => {
+						return (
+							<React.Fragment key={history.created_at}>
+								{/* ထား */}
+								{history.status === "pawn" ? (
+									<Grid item xs={12}>
+										<Stack>
+											<Typography variant='subtitle2'>
+												{history.name}
+												<Typography variant='body' sx={{ fontSize: "1rem" }}>(အပေါင်ထား)</Typography>
+											</Typography>
+											<Typography variant='body2'>{history.gold}</Typography>
+											<Typography variant='body2'>{CalculateWeight(history.weight)}</Typography>
+											<Typography variant='body2'>{NumChangeEngToMM(history.price, true)} ကျပ်တိတိ</Typography>
+											<Typography variant='body2'>{GetMMDate(new Date(history.date))}</Typography>
+											<Typography variant='body2'>--{history.description}--</Typography>
+										</Stack>
+									</Grid>
+								) : null}
+
+								{/* ထပ်ယူ */}
+								{history.status === "htet_yu" ? (
+									<Grid item xs={12}>
+										<Stack>
+											<Typography variant='subtitle2'>
+												{history.name}
+												<Typography variant='body' sx={{ fontSize: "1rem" }}>(ထပ်ယူ)</Typography>
+											</Typography>
+											<Typography variant='body2'>{history.gold}</Typography>
+											<Typography variant='body2'>{CalculateWeight(history.weight)}</Typography>
+											<Typography variant='body2'>{NumChangeEngToMM(history.price, true)} ကျပ်တိတိ</Typography>
+											<Typography variant='body2'>{GetMMDate(new Date(history.date))}</Typography>
+											<Typography variant='body2'>--{history.description}--</Typography>
+										</Stack>
+									</Grid>
+								) : null}
+
+								{/* အတိုးဆပ် */}
+								{history.status === "pay_interest" ? (
+									<Grid item xs={12}>
+										<Stack>
+											<Typography variant='subtitle2'>
+												{history.name}
+												<Typography variant='body' sx={{ fontSize: "1rem" }}>(အတိုးဆပ်)</Typography>
+											</Typography>
+											<Typography variant='body2'>{NumChangeEngToMM(history.pay_price, true)} ကျပ်တိတိ (ဆပ်)</Typography>
+											<Typography variant='body2'>{NumChangeEngToMM(history.left_price, true)} ကျပ်တိတိ (ကျန်)</Typography>
+											<Typography variant='body2'>{GetMMDate(new Date(history.pay_date))} (ဆပ်)</Typography>
+											<Typography variant='body2'>{GetMMDate(new Date(history.change_date))} (ပြောင်း)</Typography>
+											<Typography variant='body2'>--{history.description}--</Typography>
+										</Stack>
+									</Grid>
+								) : null}
+
+								{/* ခွဲရွေး */}
+								{history.status === "half_redeem" ? (
+									<Grid item xs={12}>
+										<Stack>
+											<Typography variant='subtitle2'>
+												{history.name}
+												<Typography variant='body' sx={{ fontSize: "1rem" }}>(ခွဲရွေး)</Typography>
+											</Typography>
+											<Typography variant='body2'>{history.take_gold} (ရွေး)</Typography>
+											<Typography variant='body2'>{history.left_gold} (ကျန်)</Typography>
+											<Typography variant='body2'>{CalculateWeight(history.weight)}</Typography>
+											<Typography variant='body2'>{NumChangeEngToMM(history.pay_price, true)} ကျပ်တိတိ (သွင်း)</Typography>
+											<Typography variant='body2'>{NumChangeEngToMM(history.left_price, true)} ကျပ်တိတိ (ကျန်)</Typography>
+											<Typography variant='body2'>{GetMMDate(new Date(history.date))}</Typography>
+											{/* <Typography variant='body2'>{GetMMDate(new Date(history.change_date))} (ပြောင်း)</Typography> */}
+											<Typography variant='body2'>--{history.description}--</Typography>
+										</Stack>
+									</Grid>
+								) : null}
+
+								{/* ရွေး */}
+								{history.status === "redeem" ? (
+									<Grid item xs={12}>
+										<Stack>
+											<Typography variant='subtitle2'>
+												{history.name}
+												<Typography variant='body' sx={{ fontSize: "1rem" }}>(ရွေး)</Typography>
+											</Typography>
+											<Typography variant='body2'>{NumChangeEngToMM(history.price, true)} ကျပ်တိတိ</Typography>
+											<Typography variant='body2'>{GetMMDate(new Date(history.date))}</Typography>
+											<Typography variant='body2'>--{history.description}--</Typography>
+										</Stack>
+									</Grid>
+								) : null}
+							</React.Fragment>
+						)
+					})
+				}
+			</Grid>
 		</div>
 	)
 })
