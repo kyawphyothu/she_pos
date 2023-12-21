f6535b72b74cc389dad0773d62fdde6fa5c25e77
kyawphyothu <kyawphyothukpt256@gmail.com>
2023-12-21 23:16:19 +0630

    migrate file update4

--- api/migration/acceptors.sql
+++ api/migration/acceptors.sql
@@ -4,3 +4,10 @@ create table acceptors(
 	short_name varchar(255) not null,
 	primary key (id)
 )
+
+insert into acceptors (id, name, short_name) VALUES
+	(1, 'အိမ်', 'အိမ်'),
+	(2, 'အေးအေးခိုင်', 'အေး'),
+	(3, 'စန်းစန်းထွေး', 'စန်း'),
+	(4, 'ဥမ္မာဝင်း', 'ဥမ္မာ')
+;
