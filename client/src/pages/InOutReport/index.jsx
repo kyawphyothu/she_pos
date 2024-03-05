95bf2b849e3320b9d10000112e543e3b2dc77f8e
kyawphyothu <kyawphyothukpt256@gmail.com>
2024-03-05 13:56:10 +0630

    report page by monthly 01

--- client/src/pages/InOutReport/index.jsx
+++ client/src/pages/InOutReport/index.jsx
@@ -19,6 +19,8 @@ export default function InOutReport() {
 	const [isFetchingData, setIsFetchingData] = useState(false);
 	const [monthlyReports, setMonthlyReports] = useState({});
 
+	console.log(monthlyReports)
+
 	const monthsArray = [
 		"January", "February", "March", "April",
 		"May", "June", "July", "August",
@@ -85,38 +87,44 @@ export default function InOutReport() {
 							) : (
 								<>
 									{
-										monthsArray.filter((i, index) => monthlyReports.hasOwnProperty(index+1)).map((month, month_index) => {
-											return (
-												<Accordion defaultExpanded>
-													<AccordionSummary>{month} - {selectedYear.getFullYear()}</AccordionSummary>
-													<AccordionDetails sx={{ overflow: "auto" }}>
-														{
-															acceptors.map((acceptor, acceptor_index) => {
-																const total_in = (+monthlyReports[month_index+1]?.[acceptor.id]?.pay_interest_price || 0) + (+monthlyReports[month_index+1]?.[acceptor.id]?.redeem_price || 0) + (+monthlyReports[month_index+1]?.[acceptor.id]?.half_redeem_price || 0);
-																const total_out = (+monthlyReports[month_index+1]?.[acceptor.id]?.htet_yu_price || 0) + (+monthlyReports[month_index+1]?.[acceptor.id]?.pawn_price || 0);
+										monthsArray.filter((i, index) => monthlyReports.hasOwnProperty(index+1)).length > 0 ? (
+											monthsArray.filter((i, index) => monthlyReports.hasOwnProperty(index+1)).map((month, month_index) => {
+												return (
+													<Accordion defaultExpanded key={month}>
+														<AccordionSummary>{month} - {selectedYear.getFullYear()}</AccordionSummary>
+														<AccordionDetails sx={{ overflow: "auto" }}>
+															{
+																acceptors.map((acceptor, acceptor_index) => {
+																	const total_in = (+monthlyReports[month_index+1]?.[acceptor.id]?.pay_interest_price || 0) + (+monthlyReports[month_index+1]?.[acceptor.id]?.redeem_price || 0) + (+monthlyReports[month_index+1]?.[acceptor.id]?.half_redeem_price || 0);
+																	const total_out = (+monthlyReports[month_index+1]?.[acceptor.id]?.htet_yu_price || 0) + (+monthlyReports[month_index+1]?.[acceptor.id]?.pawn_price || 0);
 
-																return (
-																	<>
-																		<Stack direction={"row"} spacing={2}>
-																			<Box sx={{ display: "flex", flexGrow: "1", textAlign: "center", alignItems: "center" }}>
-																				<Typography>
-																					{acceptor.short_name}
-																				</Typography>
-																			</Box>
-																			<Stack direction={"column"}>
-																				<Box color={green[500]} sx={{ display: "flex", justifyContent: "flex-end" }}>{total_in.toLocaleString()}</Box>
-																				<Box color={red[500]} sx={{ display: "flex", justifyContent: "flex-end" }}>{total_out.toLocaleString()}</Box>
+																	return (
+																		<>
+																			<Stack direction={"row"} spacing={2}>
+																				<Box sx={{ display: "flex", flexGrow: "1", textAlign: "center", alignItems: "center" }}>
+																					<Typography>
+																						{acceptor.short_name}
+																					</Typography>
+																				</Box>
+																				<Stack direction={"column"}>
+																					<Box color={green[500]} sx={{ display: "flex", justifyContent: "flex-end" }}>{total_in.toLocaleString()}</Box>
+																					<Box color={red[500]} sx={{ display: "flex", justifyContent: "flex-end" }}>{total_out.toLocaleString()}</Box>
+																				</Stack>
 																			</Stack>
-																		</Stack>
-																		{acceptors[acceptor_index+1] && <Divider />}
-																	</>
-																)
-															})
-														}
-													</AccordionDetails>
-												</Accordion>
-											)
-										})
+																			{acceptors[acceptor_index+1] && <Divider />}
+																		</>
+																	)
+																})
+															}
+														</AccordionDetails>
+													</Accordion>
+												)
+											})
+										):(
+											<>
+												<Typography textAlign={"center"} color={'GrayText'}>Empty</Typography>
+											</>
+										)
 									}
 								</>
 							)
